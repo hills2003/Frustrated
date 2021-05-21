7650df6f1fc5096566c1cfc36cf153d6b28028fd
hills <chillaryvictor@gmail.com>
2021-05-21 03:51:14 -1200

    wooow

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
       <Authprovider>
       <Switch>
              <Private  path="/home" component={Weather} />
-             <Route exact path="/" component={GoogleSignin} />
+             <Route exact path="/Sign-in" component={GoogleSignin} />
       </Switch>
       </Authprovider>
     </Router>
